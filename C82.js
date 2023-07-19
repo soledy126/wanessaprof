34e0f00cdab06974e19478a14b89346f9fe5f909
soledy126 <132954618+soledy126@users.noreply.github.com>
2023-07-19 16:02:37 -0300

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,21 @@
+console.log("Meu primeiro projeto no console")
+
+            var firstName = "Arthur"
+            var lastName = "Bernardo"
+            var fullName = firstName.concat(lastName)
+            console.log(fullName)
+
+            var a = 10
+            var b = 5
+
+            var add =a + b
+            console.log(add);
+
+            var sub =a - b
+            console.log(sub);
+
+            var multiplie =a * b
+            console.log(multiplie);
+
+            var divide =a / b
+            console.log(divide);
